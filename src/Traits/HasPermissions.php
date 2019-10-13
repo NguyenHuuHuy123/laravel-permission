c1ffb0c17e11449546cf2a67763339431221df7c
Juvenal Pengele <pengeleglodiejuvenal@gmail.com>
2019-10-13 10:32:29 +0000

    Update documentation and code to fix Style CI issues.

--- src/Traits/HasPermissions.php
+++ src/Traits/HasPermissions.php
@@ -427,14 +427,14 @@ trait HasPermissions
     }
 
     /**
-     * Check if there are all the direct permissions
+     * Check if there are all the direct permissions.
      * @param array $permissions
      * @return bool
      */
     public function hasAllDirectPermissions(array $permissions) : bool
     {
         foreach ($permissions as $permission) {
-            if(! $this->hasDirectPermission($permission)) {
+            if (! $this->hasDirectPermission($permission)) {
                 return false;
             }
         }
@@ -443,14 +443,14 @@ trait HasPermissions
     }
 
     /**
-     * Check if there are any the direct permissions
+     * Check if there are any the direct permissions.
      * @param array $permissions
      * @return bool
      */
     public function hasAnyDirectPermission(array $permissions) : bool
     {
         foreach ($permissions as $permission) {
-            if($this->hasDirectPermission($permission)) {
+            if ($this->hasDirectPermission($permission)) {
                 return true;
             }
         }
